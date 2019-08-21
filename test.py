d9d56b4f0dc4bff16d35ac202f95bb969f717aaf
Boping Chen <chen.boping@cern.ch>
2019-08-21 11:24:38 -0700

    add the python file

---
+++ test.py
@@ -0,0 +1,11 @@
+from mean import *
+def test():
+    list=[3,4,5]
+    obs=mean(list)
+    exp=4
+    assert obs==exp
+def test1():
+    list=[0,4,5]
+    obs=mean(list)
+    exp=4
+    assert obs==exp
